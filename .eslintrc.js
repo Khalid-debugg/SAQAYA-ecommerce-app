6c04a4f491ea76e827127fba49c6f4b2d20070cc
Khalid-debugg <khalidsherif.dev@gmail.com>
2026-03-30 22:15:27 +0200

    chore: syncing Eslint with prettier config files and reformatting the project files

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,16 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        semi: false,
+        singleQuote: false,
+        trailingComma: "es5",
+        printWidth: 80,
+      },
+    ],
+
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
@@ -27,4 +37,4 @@ module.exports = {
       },
     },
   ],
-};
+}
