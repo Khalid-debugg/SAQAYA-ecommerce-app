3419894a4ea1c9a57a96cc1d2ca541c9d332fa79
Khalid-debugg <khalidsherif.dev@gmail.com>
2026-04-01 21:14:31 +0200

    feat: add a lazy loaded default layout with a boilerplated Header and footer Components

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/layouts/Header.vue"
+import Footer from "@/components/layouts/Footer.vue"
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,41 +5,53 @@ Vue.use(VueRouter)
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "home",
-    component: () =>
-      import(/* webpackChunkName: "home" */ "@/views/HomeView.vue"),
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "@/views/AboutView.vue"),
-  },
-  {
-    path: "/contact",
-    name: "contact",
-    component: () =>
-      import(/* webpackChunkName: "contact" */ "@/views/ContactView.vue"),
-  },
-  {
-    path: "/products",
-    name: "products",
-    component: () =>
-      import(/* webpackChunkName: "products" */ "@/views/ProductsView.vue"),
-  },
-  {
-    path: "/product/:id",
-    name: "product-details",
+    name: "default-layout",
     component: () =>
       import(
-        /* webpackChunkName: "product-details" */ "@/views/ProductDetailsView.vue"
+        /* webpackChunkName: "default-layout" */ "@/layouts/DefaultLayout.vue"
       ),
-  },
-  {
-    path: "*",
-    name: "not-found",
-    component: () =>
-      import(/* webpackChunkName: "not-found" */ "@/views/NotFoundView.vue"),
+    children: [
+      {
+        path: "",
+        name: "home",
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/HomeView.vue"),
+      },
+      {
+        path: "about",
+        name: "about",
+        component: () =>
+          import(/* webpackChunkName: "about" */ "@/views/AboutView.vue"),
+      },
+      {
+        path: "contact",
+        name: "contact",
+        component: () =>
+          import(/* webpackChunkName: "contact" */ "@/views/ContactView.vue"),
+      },
+      {
+        path: "products",
+        name: "products",
+        component: () =>
+          import(/* webpackChunkName: "products" */ "@/views/ProductsView.vue"),
+      },
+      {
+        path: "product/:id",
+        name: "product-details",
+        component: () =>
+          import(
+            /* webpackChunkName: "product-details" */ "@/views/ProductDetailsView.vue"
+          ),
+      },
+      {
+        path: "*",
+        name: "not-found",
+        component: () =>
+          import(
+            /* webpackChunkName: "not-found" */ "@/views/NotFoundView.vue"
+          ),
+      },
+    ],
   },
 ]
 
