0870840e349c2ffb92b0c73a8485433cf63d9eca
Khalid-debugg <khalidsherif.dev@gmail.com>
2026-04-02 02:43:14 +0200

    feat: add icons setup with registry pattern

---
+++ src/components/icons/AppIcon.vue
@@ -0,0 +1,36 @@
+<template>
+  <component :is="icon" :width="size" :height="size" class="app-icon" />
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import { icons, IconName } from "./index"
+
+export default Vue.extend({
+  name: "AppIcon",
+
+  props: {
+    name: {
+      type: String as () => IconName,
+      required: true,
+    },
+    size: {
+      type: Number,
+      default: 24,
+    },
+  },
+
+  computed: {
+    icon() {
+      return icons[this.name as string as IconName] ?? icons["close"]
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.app-icon {
+  display: block;
+  flex-shrink: 0;
+}
+</style>
---
+++ src/components/icons/CloseIcon.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+    :width="width"
+    :height="height"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M18 6L6 18"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+    <path
+      d="M6 6L18 18"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+export default Vue.extend({
+  name: "CloseIcon",
+  props: {
+    width: { type: Number, default: 24 },
+    height: { type: Number, default: 24 },
+  },
+})
+</script>
