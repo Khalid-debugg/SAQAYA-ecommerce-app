f518b26ba3da0b7ec72ca4c497ef6428a6028016
Khalid-debugg <khalidsherif.dev@gmail.com>
2026-04-05 20:50:51 +0200

    refactor: simplify component structure by removing config file

--- src/components/layouts/CartSideBar/CartSideBar.vue
+++ src/components/layouts/CartSideBar/CartSideBar.vue
@@ -100,8 +100,18 @@
 <script lang="ts">
 import Vue from "vue"
 import AppIcon from "@/components/icons/AppIcon.vue"
-import { PaymentMethod, bankLogos } from "./config"
+type PaymentMethod = "bank" | "cash"
+type BankLogo = { name: string; fileName: string }
 
+export const bankLogos: BankLogo[] = [
+  { name: "B kash", fileName: "b-kash" },
+  { name: "Visa", fileName: "visa" },
+  { name: "Master Card", fileName: "master-card" },
+  { name: "Indian Bank", fileName: "indian-bank" },
+].map((logo) => ({
+  ...logo,
+  src: require(`@/assets/images/logos/${logo.fileName}.png`),
+}))
 export default Vue.extend({
   name: "CartSideBar",
 
