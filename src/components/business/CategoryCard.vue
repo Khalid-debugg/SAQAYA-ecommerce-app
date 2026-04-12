b9ec0bcc5078b09ecc979ce058b15544c820d181
Khalid-debugg <khalidsherif.dev@gmail.com>
2026-04-13 00:15:54 +0200

    feat: add category card component

---
+++ src/components/business/CategoryCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="category-card" @click="navigate">
+    <app-icon :name="category.slug" :width="56" :height="56" />
+    <span class="category-card__name">{{ category.name }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { ProductCategory } from "@/types/product"
+import Vue, { PropType } from "vue"
+import AppIcon from "../ui/AppIcon.vue"
+
+export default Vue.extend({
+  name: "CategoryCard",
+  components: { AppIcon },
+  props: {
+    category: {
+      type: Object as PropType<ProductCategory>,
+      required: true,
+    },
+  },
+  methods: {
+    navigate() {
+      this.$router.push({
+        name: "products",
+        query: { category: this.category.slug },
+      })
+    },
+  },
+})
+</script>
