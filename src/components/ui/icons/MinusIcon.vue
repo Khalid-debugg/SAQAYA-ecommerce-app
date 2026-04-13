e43db3c1e90abbab2f7b335004974b37dd381f70
Khalid-debugg <khalidsherif.dev@gmail.com>
2026-04-14 01:56:27 +0200

    feat: add details icons and modify arrow down icon color

---
+++ src/components/ui/icons/MinusIcon.vue
@@ -0,0 +1,35 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 18 2"
+    fill="currentColor"
+  >
+    <path
+      d="M16.75 0.75H0.75"
+      stroke="currentColor"
+      stroke-width="1.5"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  name: "MinusIcon",
+
+  props: {
+    width: {
+      type: Number,
+      default: 24,
+    },
+    height: {
+      type: Number,
+      default: 24,
+    },
+  },
+})
+</script>
