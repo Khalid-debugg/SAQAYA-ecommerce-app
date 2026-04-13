b11e7715e291f94ea06a12c9c431117165f4ca6c
Khalid-debugg <khalidsherif.dev@gmail.com>
2026-04-13 13:46:40 +0200

    feat: add app error component

---
+++ src/components/ui/AppError.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="app-error">
+    <p class="app-error__message">{{ message }}</p>
+    <app-button modifier="outline" @click.native="$emit('retry')">
+      Try again
+    </app-button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+import AppButton from "@/components/ui/AppButton.vue"
+
+export default Vue.extend({
+  name: "AppError",
+  components: { AppButton },
+  props: {
+    message: {
+      type: String,
+      default: "Something went wrong. Please try again.",
+    },
+  },
+})
+</script>
