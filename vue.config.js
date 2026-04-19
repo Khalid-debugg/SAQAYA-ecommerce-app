7a6d8d084ed172ba5e245f379aafbcd0b046b80c
Khalid-debugg <khalidsherif.dev@gmail.com>
2026-04-19 15:05:16 +0200

    chore: install @vue/compat and configure migration build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require("@vue/cli-service")
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    config.resolve.alias.set("vue", "@vue/compat")
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => ({
+        ...options,
+        compilerOptions: {
+          compatConfig: { MODE: 2 },
+        },
+      }))
+  },
 })
